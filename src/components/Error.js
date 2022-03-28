bc90a1632d78419a1b69bc75ee3c05f90d6f49fc
Ankur-cell-coder <ankurbgs6243@gmail.com>
2022-03-28 22:16:10 +0530

    error page is set-up

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import Styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
 
 function Error() {
+    const navigate=useNavigate()
   return (
-    <>
-    <h1>404</h1>
-    <h1>Page not found!</h1>
-    </>
+    <Error1>
+    <h1 style={{color:"#544598",marginTop:"200px"}}>404</h1>
+    <h1 style={{color:"#544598"}}>Page not found!</h1>
+    <button onClick={()=>navigate('/')} style={{width:"100px",height:"40px",color:"#544598",background:"#E2F3F5"}}>Home</button>
+    </Error1>
   )
 }
 
 export default Error
+
+const Error1 = Styled.div`
+ margin-top:-22px;
+ height:743px;
+ background:#C4EBEF;
+ display:flex;
+ flex-direction:column;
+ align-items:center;
+`;
+
