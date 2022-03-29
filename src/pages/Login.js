09fed0b082b49b914225397449c1af13e691cbd9
Ankur-cell-coder <ankurbgs6243@gmail.com>
2022-03-29 12:30:14 +0530

    login page created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import loginimage from '../images/loginimage.jpg'
 
 function Login() {
   return (
-    <div>
-        <h1>Login</h1>
-    </div>
+    <Login1>
+      <div className='container'>
+        <img src={loginimage} style={{height:"500px",marginTop:"30px"}}/>
+        <h1 style={{marginLeft:"630px",color:"#9CDAF1"}}>Search Github User</h1>
+        <button style={{width:"150px",marginLeft:"700px",color:"white",background:"#544598",border:"2px solid #544598"}}>Login/Signup</button>
+      </div>
+    </Login1>
   )
 }
 
 export default Login
+
+const Login1= styled.div`
+  .container{
+    display:flex;
+    flex-direction:column;
+  }
+`;
