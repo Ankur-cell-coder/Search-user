067262b102945334a9bcf60910f3aa06f0f9e8b3
Ankur-cell-coder <ankurbgs6243@gmail.com>
2022-04-26 23:22:36 +0530

    search functionality and error functionality implemented

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,8 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
 
@@ -11,8 +12,51 @@ const GithubProvider = ({ children }) => {
     const [githubUser,setGithubUser]=useState(mockUser);
     const [repos,setRepos]=useState(mockRepos);
     const [followers,setFollowers]=useState(mockFollowers);
+    //request loading 
+    const [requests,setRequests]=useState(0);
+    const [loading,setIsLoading] =useState(false);
+    //error
+    const [error,setError]=useState({show:false,msg:''});
+
+    const searchGithubUser = async (user) =>{
+      toggleError()
+      // console.log(user);
+      const response = await axios(`${rootUrl}/users/${user}`).
+      catch((err)=>
+      console.log(err)
+      );
+      console.log(response);
+      if(response){
+        setGithubUser(response.data);
+      }else{
+        toggleError(true,'there is no user with that username');
+      }
+    };
+
+    //check rate 
+    const checkRequests = () =>{
+      axios(`${rootUrl}/rate_limit`)
+      .then(({data})=>{
+        let {
+          rate:{remaining},
+        } =data;
+        //  remaining=0;
+        setRequests(remaining);
+        if(remaining===0)
+        {
+          //throw an error
+          toggleError(true,'sorry,you have exeeded your hourly rate limit!');
+        }
+      })
+      .catch((err)=>console.log(err));
+    };
+    function toggleError(show=false,msg=''){
+      setError({show,msg});
+    }
+    //error
+    useEffect(()=>{checkRequests();},[]);
   return(
-   <GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>
+   <GithubContext.Provider value={{githubUser,repos,followers,requests,error,searchGithubUser}}>{children}</GithubContext.Provider>
   );
 };
 export {GithubProvider,GithubContext};
